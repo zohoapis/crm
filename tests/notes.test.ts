fdc23332dfc8675f9b40c23aa697ddddd5d561e9
Doug <dmick92@gmail.com>
2023-06-12 12:07:13 -0400

    feat: Added note deletion

--- tests/notes.test.ts
+++ tests/notes.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from "vitest";
 import { notes } from "~/index";
 
 notes.authToken =
-  "1000.1e406e43568d507b7c0aca7a63ac5d41.4d363acc4a74a35b0bab3452a9e4e53b";
+  "1000.966893410e942c8418ea5377e713c093.1ed7f9c5514d3f7a1f180bbb730a82c0";
 
 describe("Get Notes", () => {
   it("getNotes", async () => {
@@ -62,56 +62,68 @@ describe("Get Notes", () => {
 // });
 //});
 
-describe("Update Notes", () => {
-  // it("UpdateNotes", async () => {
-  //   expect(
-  //     await notes.UpdateNotes("3746689000002465011", {
-  //       Note_Title: "Test 3",
-  //       Note_Content: "API Test 3",
-  //     })
-  //   ).toBeTypeOf("object");
-  // });
-  // it("UpdateNotes", async () => {
-  //   expect(
-  //     await notes.UpdateNotes("Contacts", "3746689000000208014", [
-  //       {
-  //         id: "3746689000002455197",
-  //         Note_Title: "asdasd 2",
-  //         Note_Content: "API Test 2",
-  //       },
-  //       {
-  //         id: "3746689000002465011",
-  //         Note_Title: "Test 4",
-  //         Note_Content: "API Test 2",
-  //       },
-  //     ])
-  //   ).toBeTypeOf("object");
-  // });
-  it("UpdateNotes", async () => {
-    expect(
-      await notes.UpdateNotes([
-        {
-          id: "3746689000002459015",
-          Note_Title: "Testing",
-          Note_Content: "API Test",
-          Parent_Id: "3746689000000208014",
-          se_module: "Contacts",
-        },
-        {
-          id: "3746689000002469022",
-          Note_Title: "Testing",
-          Note_Content: "API Test",
-          Parent_Id: "3746689000000208014",
-          se_module: "Contacts",
-        },
-        {
-          id: "3746689000002469025",
-          Note_Title: "Testing",
-          Note_Content: "API Test",
-          Parent_Id: "3746689000000208014",
-          se_module: "Contacts",
-        },
-      ])
-    ).toBeTypeOf("object");
-  });
-});
+// describe("Update Notes", () => {
+//   // it("UpdateNotes", async () => {
+//   //   expect(
+//   //     await notes.UpdateNotes("3746689000002465011", {
+//   //       Note_Title: "Test 3",
+//   //       Note_Content: "API Test 3",
+//   //     })
+//   //   ).toBeTypeOf("object");
+//   // });
+//   // it("UpdateNotes", async () => {
+//   //   expect(
+//   //     await notes.UpdateNotes("Contacts", "3746689000000208014", [
+//   //       {
+//   //         id: "3746689000002455197",
+//   //         Note_Title: "asdasd 2",
+//   //         Note_Content: "API Test 2",
+//   //       },
+//   //       {
+//   //         id: "3746689000002465011",
+//   //         Note_Title: "Test 4",
+//   //         Note_Content: "API Test 2",
+//   //       },
+//   //     ])
+//   //   ).toBeTypeOf("object");
+//   // });
+//   // it("UpdateNotes", async () => {
+//   //   expect(
+//   //     await notes.updateNotes([
+//   //       {
+//   //         id: "3746689000002459015",
+//   //         Note_Title: "Testing",
+//   //         Note_Content: "API Test",
+//   //         Parent_Id: "3746689000000208014",
+//   //         se_module: "Contacts",
+//   //       },
+//   //       {
+//   //         id: "3746689000002469022",
+//   //         Note_Title: "Testing",
+//   //         Note_Content: "API Test",
+//   //         Parent_Id: "3746689000000208014",
+//   //         se_module: "Contacts",
+//   //       },
+//   //       {
+//   //         id: "3746689000002469025",
+//   //         Note_Title: "Testing",
+//   //         Note_Content: "API Test",
+//   //         Parent_Id: "3746689000000208014",
+//   //         se_module: "Contacts",
+//   //       },
+//   //     ])
+//   //   ).toBeTypeOf("object");
+//   // });
+// });
+
+// describe("Delete Notes", () => {
+//   it("deleteNotes", async () => {
+//     expect(
+//       await notes.deleteNotes(
+//         "Contacts",
+//         "3746689000000208014",
+//         "3746689000002465011"
+//       )
+//     ).toBeTypeOf("object");
+//   });
+// });
